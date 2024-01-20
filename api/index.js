7c758d23a29b1875e7e6cfdf21391a2b70e7084b
Kadjo Claude <kadjobubbleup@gmail.com>
2024-01-20 21:43:33 +0100

    1er route test

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const userRoutes = require("../routes/userRoute")
 //Configuration des variables d'env
 dotenv.config()
 
@@ -12,8 +12,10 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express();
 
 
+app.use("/api/user", userRoutes)
 
 
 app.listen(3000, () => {
     console.log(`Le serveur est lancé sur le port 3000`)
 });
+ 
