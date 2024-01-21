8c236879be959f1212ae0237c870c2145e1c9ba3
Kadjo Claude <kadjobubbleup@gmail.com>
2024-01-21 01:15:09 +0100

    mise en place de errorHandler

--- api/index.js
+++ api/index.js
@@ -18,6 +18,15 @@ app.use(express.json());
 app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500; 
+    const message = err.message || "Erreur du serveur";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 
 app.listen(3000, () => {
     console.log(`Le serveur est lancé sur le port 3000`)
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,12 @@
 const User = require("../models/userModel")
 const bcryptjs = require("bcryptjs")
+const {errorHandler} = require("../utils/error")
 
-
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const {username, email , password} = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({message : "Veuillez remplir tous les champs!"})
+        next(errorHandler(400, "Veuillez remplir tous les champs!"))
     }
 
     const hashPassword =  bcryptjs.hashSync(password, 10);
@@ -20,7 +20,7 @@ const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({message : "User enregisré avec succès !"})
     }catch(e) {
-        res.status(500).json({message : e.message})
+        next(e)
     }
 }
 
