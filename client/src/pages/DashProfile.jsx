87f78375229194a0256b24309e6c39c9c21b4c43
Kadjo Claude <kadjobubbleup@gmail.com>
2024-01-23 13:52:47 +0100

    Finalisation du design du profil dashbord

--- client/src/pages/DashProfile.jsx
+++ client/src/pages/DashProfile.jsx
@@ -1,9 +1,40 @@
-
-
+import {useSelector} from "react-redux"
+import {Button, TextInput} from "flowbite-react"
 const DashProfile = () => {
+    const {currentUser} = useSelector((state) => state.user)
+    console.log(currentUser)
   return (
-    <div>
-      
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profil</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+            <img src={currentUser.rest.profilePicture} alt="user" className="rounded-full w-full object-cover border-8 border-[lightgray]" />
+        </div>
+        <TextInput
+            type="text"
+            id="username"
+            placeholder="username"
+            defaultValue = {currentUser.rest.username}
+        />
+        <TextInput
+            type="email"
+            id="email"
+            placeholder="email"
+            defaultValue = {currentUser.rest.email}
+        />
+        <TextInput
+            type="password"
+            id="password"
+            placeholder="***********"
+        />
+        <Button type="submit" outline gradientDuoTone="purpleToBlue">
+            Modifiez
+        </Button>
+        <div className="flex justify-between text-red-500 mt-5">
+            <span className="cursor-pointer">Supprimez ce compte</span>
+            <span className="cursor-pointer">Se déconnecter</span>
+        </div>
+      </form>
     </div>
   )
 }
